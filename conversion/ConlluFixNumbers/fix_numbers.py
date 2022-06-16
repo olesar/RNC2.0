3c1fe0c91214cf58f34c7cac2ee30c968f348bea
Ребриков Стефан/Rebrikov Stefan <robstef85@gmail.com>
2022-06-16 22:49:30 +0300

    Update/ add single file functionality

--- conversion/ConlluFixNumbers/fix_numbers.py
+++ conversion/ConlluFixNumbers/fix_numbers.py
@@ -4,60 +4,10 @@ import sys
 import os
 
 
-def main():
-    parser = argparse.ArgumentParser(description='fixing numeration')
-    parser.add_argument('folder_path', type=str, help='folder path')
-    parser.add_argument('new_folder_path', type=str, help='new folder path')
-    args = parser.parse_args()
-
-    if len(sys.argv) != 3:
-        print('fix_numbers.py  <folder_path> <new_folder_path>')
-        sys.exit(-1)
-
-    folder_path = args.folder_path
-    new_folder_path = args.new_folder_path
-
-    for path, subdirs, files in os.walk(folder_path):
-        for name in files:
-            file_path = os.path.join(path, name)
-            new_file_path = new_folder_path + "/" + "/".join(file_path.split("/")[5:])
-
-            new_file_subfolder_path = "/".join(new_file_path.split("/")[:-1])
-            print(new_file_subfolder_path)
-
-            if not os.path.exists(new_file_subfolder_path):
-                os.makedirs(new_file_subfolder_path)
-
-            with open(file_path, 'r', encoding="utf-8") as infile, open(new_file_path, 'w',
-                                                                        encoding='utf-8') as outfile:
-                file_text_list = infile.read().strip('\n').split('\n\n')
-                for paragraph in file_text_list:
-                    header = []
-                    body = []
-                    paragraph_text = paragraph.split('\n')
-                    if paragraph_text[0] != "":
-                        for line in paragraph_text:
-                            # print(line)
-                            if len(line) > 0:
-                                if re.search('[0-9]', line[0]):
-                                    body.append(line.strip('\n').split('\t'))
-                                else:
-                                    header.append(line.strip('\n'))
-                    # print('header', '\n', header)
-                    # print('body', '\n', body)
-                    for header_line in header:
-                        outfile.write(header_line + '\n')
-                    counter = 1
-                    for body_line in body:
-                        outfile.write(str(counter) + '\t' + "\t".join(body_line[1:]) + '\n')
-                        counter += 1
-                    outfile.write('\n')
-
-
-def fix_test_windows():
-    file_path = 'test/bogatej1_001.conllu'
-    new_file_path = 'new_folder/bogatej1_001.conllu'
-    with open(file_path, 'r', encoding="utf-8") as infile, open(new_file_path, 'w', encoding='utf-8') as outfile:
+def do_single(file_path, out_file_path):
+    os.makedirs(os.path.dirname(out_file_path), exist_ok=True)
+    with open(file_path, 'r', encoding="utf-8") as infile, open(out_file_path, 'w',
+                                                             encoding='utf-8') as outfile:
         file_text_list = infile.read().strip('\n').split('\n\n')
         for paragraph in file_text_list:
             header = []
@@ -71,8 +21,6 @@ def fix_test_windows():
                             body.append(line.strip('\n').split('\t'))
                         else:
                             header.append(line.strip('\n'))
-            # print('header', '\n', header)
-            # print('body', '\n', body)
             for header_line in header:
                 outfile.write(header_line + '\n')
             counter = 1
@@ -82,6 +30,36 @@ def fix_test_windows():
             outfile.write('\n')
 
 
+def do_multiple(root, result_folder_path):
+    walk = [(x, y, z) for x, y, z in os.walk(root)]
+    for root, dirs, files in walk:
+        for file in range(len(files)):
+            new_file_path = os.path.join(result_folder_path + root.replace(root, ""), files[file])
+            # print(new_file_path)
+            f_path = os.path.join(root, files[file])
+            # print(f_path)
+            do_single(f_path, new_file_path)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='fixing numeration')
+    parser.add_argument('single_or_folder', type=str, help='if file is single or folder')
+    parser.add_argument('source_path', type=str, help='source_path path')
+    parser.add_argument('result_path', type=str, help='result_path')
+    args = parser.parse_args()
+
+    if len(sys.argv) != 4:
+        sys.exit(-1)
+
+    if args.single_or_folder == 'single':
+        do_single(args.source_path, args.result_path)
+    elif args.single_or_folder == 'folder':
+        do_multiple(args.source_path, args.result_path)
+    else:
+        print('fix_numbers.py <single/folder> <filepath/root_dir> <output_path/output_dir>')
+        print('example for single file: fix_numbers.py single test_file.conllu result_file.conllu')
+        print('example for folder: fix_numbers.py folder test_folder result_folder')
+
+
 if __name__ == '__main__':
-    # fix_test_windows()
     main()
